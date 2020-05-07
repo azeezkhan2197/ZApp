bccd8f2a68c2dbfad5cbca10c2bb474946aeed0d
azeezkhan2197 <abdulazeez2197@gmail.com>
2020-05-07 12:56:23 +0530

    commiting java files

---
+++ SpringJava/zapp/src/main/java/com/app/zapp/ZappApplication.java
@@ -0,0 +1,13 @@
+package com.app.zapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZappApplication.class, args);
+	}
+
+}
---
+++ SpringJava/zapp/src/test/java/com/app/zapp/ZappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.zapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
